ccd3e488ba86394992359981110bb05aaaf85c1d
Wojciech Brüggemann <wojtek77@o2.pl>
2016-09-24 07:36:33 +0200

    add prefix "/admin" to route for CRUDs

--- config/routes.php
+++ config/routes.php
@@ -43,6 +43,12 @@ use Cake\Routing\Route\DashedRoute;
  */
 Router::defaultRouteClass(DashedRoute::class);
 
+Router::prefix('admin', function ($routes) {
+    // All routes here will be prefixed with `/admin`
+    // And have the prefix => admin route element added.
+    $routes->fallbacks(DashedRoute::class);
+});
+
 Router::scope('/', function (RouteBuilder $routes) {
     /**
      * Here, we are connecting '/' (base path) to a controller called 'Pages',
